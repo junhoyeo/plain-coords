5c55927708e4ce804691664920bb3eb132508254
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-31 10:50:20 +0900

    Fix usage in new dist function

--- new_dist.py
+++ new_dist.py
@@ -3,4 +3,10 @@ from dot import *
 def D(A, B):
     return A != B
 
-main()
+for A in random_dots():
+  for B in random_dots():
+      for C in random_dots():
+          if not validator(D, A, B, C):
+              print(False)
+              exit(0)
+print(True)
